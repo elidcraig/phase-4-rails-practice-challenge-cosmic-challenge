68b33385430d1883faa525b2ede9acf5aeba26a1
Eli Craig <elidcraig@gmail.com>
2022-07-05 08:32:05 -0700

    custom error handling

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,14 @@
 class ApplicationController < ActionController::API
+  rescue_from ActiveRecord::RecordInvalid, with: :handle_invalid
+  rescue_from ActiveRecord::RecordNotFound, with: :handle_not_found
+
+  private
+
+  def handle_invalid e
+    render json: { error: e.messages }, status: 422
+  end
+
+  def handle_not_found e
+    render json: { error: "#{e.model} not found" }, status: 404
+  end
 end
