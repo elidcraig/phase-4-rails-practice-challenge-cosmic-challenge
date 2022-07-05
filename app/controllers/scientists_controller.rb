ce6dcb2feef9b2930aed75a5a160fc16dcff6217
Eli Craig <elidcraig@gmail.com>
2022-07-05 08:40:00 -0700

    render planets with scientists#show

--- app/controllers/scientists_controller.rb
+++ app/controllers/scientists_controller.rb
@@ -1,2 +1,15 @@
 class ScientistsController < ApplicationController
+  def index
+    render json: Scientist.all
+  end
+
+  def show
+    render json: find_scientist, serializer: ScientistPlanetsSerializer
+  end
+
+  private
+
+  def find_scientist
+    Scientist.find(params[:id])
+  end
 end
